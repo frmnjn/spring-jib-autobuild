74b90b9d6e516521a54740f7b949ce56286a6092
Ubuntu <ubuntu@ip-172-31-20-202.ap-southeast-1.compute.internal>
2020-03-13 16:51:43 +0000

    update

--- src/main/java/docker/demo/controller/MovieController.java
+++ src/main/java/docker/demo/controller/MovieController.java
@@ -21,7 +21,7 @@ public class MovieController {
 
     @GetMapping("/welcome")
     public String welcome(){
-        return "Hello World Updated!";
+        return "Hello World Updated2!";
     }
 
     @PostMapping
